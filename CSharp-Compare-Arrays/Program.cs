95fa09982bb922603dab118f8f1da7f7408e961f
Hidequel Puga <bounty31k@outlook.com>
2018-05-08 13:36:33 -0500

    First Commit

--- CSharp-Compare-Arrays/Program.cs
+++ CSharp-Compare-Arrays/Program.cs
@@ -10,6 +10,33 @@ namespace CSharp_Compare_Arrays
     {
         static void Main(string[] args)
         {
+            //populate the arrays
+            string[] array_A = { "Dog", "Wolf", "Flamingo", "Spider", "?", "7", "Orange", "Android" };
+            string[] array_B = { "Fox", "Wolf", "Bear", "7", "Cheese", "Spider", "Cat", "Linux" };
+
+            bool found_switch = false;
+
+            for (int i = 0; i < array_A.Length; i++)
+            {
+                for (int j = 0; j < array_B.Length; j++)
+                {
+                    if (array_A[i] == array_B[j])
+                    {
+                        found_switch = true;
+                        Console.WriteLine("array_A element \"" + array_A[i] + "\" was found in array_B");
+                    }
+                }
+
+                if (found_switch == false)
+                {
+                    Console.WriteLine("array_A element \"" + array_A[i] + "\" was not found in array_B");
+                }
+
+                found_switch = false;
+            }
+
+            Console.Write("Press Any Key to exit...");
+            Console.ReadKey();
         }
     }
 }
